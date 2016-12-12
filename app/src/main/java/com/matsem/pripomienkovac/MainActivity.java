081dbcecdc4198958d62d3befe9bdc06e8db56ec
Tiboris <tibor.dudlak@gmail.com>
2016-12-12 23:19:07 +0100

    beautiflied main screen again to same size

--- app/src/main/java/com/matsem/pripomienkovac/MainActivity.java
+++ app/src/main/java/com/matsem/pripomienkovac/MainActivity.java
@@ -37,12 +37,13 @@ public class MainActivity extends AppCompatActivity {
 		// Making notification bar transparent but something is missing not transparent
 //		if (Build.VERSION.SDK_INT >= 21) {
 //			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-//			changeStatusBarColor();
+//
 //		}
 
 		setContentView(R.layout.activity_main);
 		notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+		//changeStatusBarColor();
 
 		btnMorning = (FrameLayout) findViewById(R.id.btn_main_1);
 		btnNoon = (FrameLayout) findViewById(R.id.btn_main_2);
@@ -132,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
 		alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
 	}
 
-	private PendingIntent getNotificationIntent() {
+	public PendingIntent getNotificationIntent() {
 		//TakePillsActivity take = new TakePillsActivity();
 		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
 		stackBuilder.addParentStack(TakePillsActivity.class);
